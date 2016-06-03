8e42fb2b498e062d329401a48cbd765ed196e67c
refuil <refuil@hotmail.com>
2016-06-03 18:04:00 +0800

    18.00

---
+++ 15.1-15.42.cc
@@ -0,0 +1,173 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+#define NUM134
+
+/*13.1*/
+/*13.4*/
+
+
+int main(){
+/*13.1*/
+#ifdef NUM131
+
+#endif
+/*13.2*/
+#ifdef NUM132
+
+#endif
+/*13.3*/
+#ifdef NUM133
+
+#endif
+/*13.4*/
+#ifdef NUM134
+
+#endif
+/*13.5*/
+#ifdef NUM135
+
+#endif
+/*13.6*/
+#ifdef NUM136
+
+#endif
+/*13.7*/
+#ifdef NUM137
+
+#endif
+/*13.13*/
+#ifdef NUM138
+
+#endif
+/*13.13*/
+#ifdef NUM1313
+
+#endif
+/*13.10*/
+#ifdef NUM1310
+
+#endif
+/*13.11*/
+#ifdef NUM1311
+
+#endif
+/*13.12*/
+#ifdef NUM1312
+
+#endif
+/*13.13*/
+#ifdef NUM1313
+
+#endif
+/*13.14*/
+#ifdef NUM1314
+
+#endif
+/*13.15*/
+#ifdef NUM1315
+
+#endif
+/*13.16*/
+#ifdef NUM1316
+
+#endif
+/*13.17*/
+#ifdef NUM1317
+
+#endif
+/*13.113*/
+#ifdef NUM1318
+
+#endif
+/*13.113*/
+#ifdef NUM13113
+
+#endif
+/*13.20*/
+#ifdef NUM1320
+
+#endif
+/*13.21*/
+#ifdef NUM1321
+
+#endif
+/*13.22*/
+#ifdef NUM1322
+
+#endif
+/*13.23*/
+#ifdef NUM1323
+
+#endif
+/*13.24*/
+#ifdef NUM1324
+
+#endif
+/*13.25*/
+#ifdef NUM1325
+
+#endif
+/*13.26*/
+#ifdef NUM1326
+
+#endif
+/*13.27*/
+#ifdef NUM1327
+
+#endif
+/*13.213*/
+#ifdef NUM1328
+
+#endif
+/*13.213*/
+#ifdef NUM13213
+
+#endif
+/*13.30*/
+#ifdef NUM1330
+
+#endif
+/*13.31*/
+#ifdef NUM1331
+
+#endif
+/*13.32*/
+#ifdef NUM1332
+
+#endif
+/*13.33*/
+#ifdef NUM1333
+
+#endif
+/*13.34*/
+#ifdef NUM1334
+
+#endif
+/*13.35*/
+#ifdef NUM1335
+
+#endif
+/*13.36*/
+#ifdef NUM1336
+
+#endif
+/*13.37*/
+#ifdef NUM1337
+
+#endif
+/*13.38*/
+#ifdef NUM1338
+
+#endif
+/*13.313*/
+#ifdef NUM13313
+
+#endif
+/*13.40*/
+#ifdef NUM1340
+
+#endif
+	return 0;
+}
